1bfec6d6a9035ff523d7b353be433679f1f4ca73
innk95 <innk95@gmail.com>
2017-07-09 19:47:47 +0300

    Поставил дизайн

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
+app_name='blogapp'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<header_id>[0-9]+)/$', views.detail, name='detail'),
